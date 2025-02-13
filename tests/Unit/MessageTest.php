d1e19e9759c46dc0393cd54b2b1dc480696cbcc3
Aleksey Kovalenko <kalexhaym@gmail.com>
2025-02-13 12:44:23 +0400

    Added Message tests

--- tests/Unit/MessageTest.php
+++ tests/Unit/MessageTest.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Kalexhaym\LaravelTelegramBot\Tests\Unit;
 
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Client\Request;
+use Illuminate\Support\Facades\Http;
 use Kalexhaym\LaravelTelegramBot\Message;
 use Orchestra\Testbench\TestCase;
 use ReflectionException;
@@ -13,6 +16,18 @@ use ReflectionException;
  */
 class MessageTest extends TestCase
 {
+    /**
+     * @var string
+     */
+    private string $testUrl = 'https://api.example.com';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->app['config']->set('telegram.api.url', $this->testUrl);
+    }
+
     /**
      * @throws ReflectionException
      */
@@ -155,5 +170,239 @@ class MessageTest extends TestCase
         ]);
         $result = $class->getCommands();
         $this->assertSame([], $result);
+    }
+
+    /**
+     * @throws ConnectionException
+     */
+    public function testSendText(): void
+    {
+        Http::fake([
+            $this->testUrl.'/sendMessage' => Http::response(['success' => true], 200),
+        ]);
+
+        $class = new Message([
+            'chat' => [
+                'id' => 1,
+            ],
+            'message_id' => 1,
+        ]);
+
+        $reply_markup = ['markup'];
+
+        $response = $class->sendText('Test Text', $reply_markup, true);
+
+        $this->assertArrayHasKey('data', $response);
+        $this->assertEquals(['success' => true], $response['data']);
+
+        Http::assertSent(function (Request $request) use ($reply_markup) {
+            return $request->url() === $this->testUrl.'/sendMessage' &&
+                $request->method() === 'POST' &&
+                $request->data() === [
+                    'chat_id'              => 1,
+                    'text'                 => 'Test Text',
+                    'disable_notification' => true,
+                    'reply_markup'         => json_encode($reply_markup),
+                ];
+        });
+    }
+
+    /**
+     * @throws ConnectionException
+     */
+    public function testSendDocument(): void
+    {
+        Http::fake([
+            $this->testUrl.'/sendDocument' => Http::response(['success' => true], 200),
+        ]);
+
+        $class = new Message([
+            'chat' => [
+                'id' => 1,
+            ],
+            'message_id' => 1,
+        ]);
+
+        $reply_markup = ['markup'];
+
+        $response = $class->sendDocument('Test Document', 'Test Caption', $reply_markup, true);
+
+        $this->assertArrayHasKey('data', $response);
+        $this->assertEquals(['success' => true], $response['data']);
+
+        Http::assertSent(function (Request $request) use ($reply_markup) {
+            return $request->url() === $this->testUrl.'/sendDocument' &&
+                $request->method() === 'POST' &&
+                $request->data() === [
+                    'chat_id'              => 1,
+                    'document'             => 'Test Document',
+                    'caption'              => 'Test Caption',
+                    'disable_notification' => true,
+                    'reply_markup'         => json_encode($reply_markup),
+                ];
+        });
+    }
+
+    /**
+     * @throws ConnectionException
+     */
+    public function testSendPhoto(): void
+    {
+        Http::fake([
+            $this->testUrl.'/sendPhoto' => Http::response(['success' => true], 200),
+        ]);
+
+        $class = new Message([
+            'chat' => [
+                'id' => 1,
+            ],
+            'message_id' => 1,
+        ]);
+
+        $reply_markup = ['markup'];
+
+        $response = $class->sendPhoto('Test Photo', 'Test Caption', $reply_markup, true);
+
+        $this->assertArrayHasKey('data', $response);
+        $this->assertEquals(['success' => true], $response['data']);
+
+        Http::assertSent(function (Request $request) use ($reply_markup) {
+            return $request->url() === $this->testUrl.'/sendPhoto' &&
+                $request->method() === 'POST' &&
+                $request->data() === [
+                    'chat_id'              => 1,
+                    'photo'                => 'Test Photo',
+                    'caption'              => 'Test Caption',
+                    'disable_notification' => true,
+                    'reply_markup'         => json_encode($reply_markup),
+                ];
+        });
+    }
+
+    /**
+     * @throws ConnectionException
+     */
+    public function testEditMessageText(): void
+    {
+        Http::fake([
+            $this->testUrl.'/editMessageText' => Http::response(['success' => true], 200),
+        ]);
+
+        $class = new Message([
+            'chat' => [
+                'id' => 1,
+            ],
+            'message_id'   => 1,
+            'reply_markup' => ['markup'],
+        ]);
+
+        $response = $class->editMessageText('Test Text');
+
+        $this->assertArrayHasKey('data', $response);
+        $this->assertEquals(['success' => true], $response['data']);
+
+        Http::assertSent(function (Request $request) {
+            return $request->url() === $this->testUrl.'/editMessageText' &&
+                $request->method() === 'POST' &&
+                $request->data() === [
+                    'chat_id'      => 1,
+                    'message_id'   => 1,
+                    'text'         => 'Test Text',
+                    'reply_markup' => json_encode(['markup']),
+                ];
+        });
+    }
+
+    /**
+     * @throws ConnectionException
+     */
+    public function testEditMessageReplyMarkup(): void
+    {
+        Http::fake([
+            $this->testUrl.'/editMessageReplyMarkup' => Http::response(['success' => true], 200),
+        ]);
+
+        $class = new Message([
+            'chat' => [
+                'id' => 1,
+            ],
+            'message_id' => 1,
+        ]);
+
+        $response = $class->editMessageReplyMarkup(['markup']);
+
+        $this->assertArrayHasKey('data', $response);
+        $this->assertEquals(['success' => true], $response['data']);
+
+        Http::assertSent(function (Request $request) {
+            return $request->url() === $this->testUrl.'/editMessageReplyMarkup' &&
+                $request->method() === 'POST' &&
+                $request->data() === [
+                    'chat_id'      => 1,
+                    'message_id'   => 1,
+                    'reply_markup' => json_encode(['markup']),
+                ];
+        });
+    }
+
+    /**
+     * @throws ConnectionException
+     */
+    public function testDeleteMessage(): void
+    {
+        Http::fake([
+            $this->testUrl.'/deleteMessage' => Http::response(['success' => true], 200),
+        ]);
+
+        $class = new Message([
+            'chat' => [
+                'id' => 1,
+            ],
+            'message_id' => 1,
+        ]);
+
+        $response = $class->deleteMessage();
+
+        $this->assertArrayHasKey('data', $response);
+        $this->assertEquals(['success' => true], $response['data']);
+
+        Http::assertSent(function (Request $request) {
+            return $request->url() === $this->testUrl.'/deleteMessage' &&
+                $request->method() === 'POST' &&
+                $request->data() === [
+                    'chat_id'    => 1,
+                    'message_id' => 1,
+                ];
+        });
+    }
+
+    /**
+     * @throws ConnectionException
+     */
+    public function testAnswerCallbackQuery(): void
+    {
+        Http::fake([
+            $this->testUrl.'/answerCallbackQuery' => Http::response(['success' => true], 200),
+        ]);
+
+        $class = new Message([
+            'chat' => [
+                'id' => 1,
+            ],
+            'message_id' => 1,
+        ]);
+
+        $response = $class->answerCallbackQuery(1);
+
+        $this->assertArrayHasKey('data', $response);
+        $this->assertEquals(['success' => true], $response['data']);
+
+        Http::assertSent(function (Request $request) {
+            return $request->url() === $this->testUrl.'/answerCallbackQuery' &&
+                $request->method() === 'POST' &&
+                $request->data() === [
+                    'callback_query_id' => 1,
+                ];
+        });
     }
 }
