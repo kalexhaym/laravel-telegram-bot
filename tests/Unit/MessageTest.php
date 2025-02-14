8bd38940a9b93a42f4340dbd7fe681d287fcff3b
kalexhaym <37374594+kalexhaym@users.noreply.github.com>
2025-02-14 10:00:16 +0000

    Fixed code style

--- tests/Unit/MessageTest.php
+++ tests/Unit/MessageTest.php
@@ -531,7 +531,7 @@ class MessageTest extends TestCase
                 $request->data() === [
                     'chat_id'         => 1,
                     'user_id'         => 1,
-                    'only_if_banned' => false,
+                    'only_if_banned'  => false,
                 ];
         });
     }
